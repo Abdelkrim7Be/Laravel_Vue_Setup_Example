1f2bbf0acbf9fc6c17997e0328eafeccc7e27896
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-07-15 19:55:44 +0100

    In welcome.blade.php, we load our vue component

---
+++ InventoPro-main/resources/js/components.vue/mainapp.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>This is my firs vue component</div>
+</template>
--- InventoPro-main/resources/views/welcome.blade.php
+++ InventoPro-main/resources/views/welcome.blade.php
@@ -7,31 +7,13 @@
 
     <title>InventoPro</title>
 
-    {{-- <link rel="stylesheet" href="{{ mix('css/all.css') }}"> --}}
-    <script>
-        (function() {
-            window.Laravel = {
-                csrfToken: '{{ csrf_token() }}'
-            };
-        })();
-    </script>
 </head>
 
 <body>
-    <div id="app">
-        @if (Auth::check())
-            <script>
-                window.authUser = {!! json_encode(Auth::user()) !!};
-                window.permissions = {!! json_encode(Auth::user()->role->permission) !!};
-            </script>
-        @else
-            <script>
-                window.authUser = null;
-            </script>
-        @endif
-    </div>
-
-    <script src="{{ mix('js/app.js') }}"></script>
+  <div id="app">
+    <mainapp></mainapp>
+  </div>
 </body>
+<script src="{{ mix('/js/app') }}"></script>
 
 </html>
